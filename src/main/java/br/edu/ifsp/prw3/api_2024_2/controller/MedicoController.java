7a714d0562e3cdf237ae2cd29f00d6f2699d59f4
Carlos Jose Pereira <carlao2005@gmail.com>
2024-10-04 18:32:38 -0300

    apos aula 2 da api

--- src/main/java/br/edu/ifsp/prw3/api_2024_2/controller/MedicoController.java
+++ src/main/java/br/edu/ifsp/prw3/api_2024_2/controller/MedicoController.java
@@ -1,14 +1,19 @@
 package br.edu.ifsp.prw3.api_2024_2.controller;
 
 import br.edu.ifsp.prw3.api_2024_2.medico.DadosCadastroMedico;
+import br.edu.ifsp.prw3.api_2024_2.medico.DadosListagemMedico;
 import br.edu.ifsp.prw3.api_2024_2.medico.Medico;
 import br.edu.ifsp.prw3.api_2024_2.medico.MedicoRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,12 +24,23 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
 
         repository.save( new Medico(dados) );
+    }
 
+    @GetMapping
+    public List<Medico> listar() {
+
+        return repository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("algunsdados")
+    public Page<DadosListagemMedico> listarAlgunsDados( Pageable paginacao )
+    {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
-
 
