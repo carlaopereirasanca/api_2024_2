132d119041d309db06cca8297db3e2c1e368eec5
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2024-11-12 16:18:24 -0300

    security aula 2 de 3

---
+++ src/main/java/br/edu/ifsp/prw3/api_2024_2/controller/AutenticacaoController.java
@@ -0,0 +1,37 @@
+package br.edu.ifsp.prw3.api_2024_2.controller;
+
+// Já vimos essas anotações em códigos anteriores:
+
+import br.edu.ifsp.prw3.api_2024_2.usuario.dadosAutenticacao;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;  // Objeto AuthenticationManager,
+                                            // que será injetado aqui:
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid dadosAutenticacao dados) {
+
+        var token = new UsernamePasswordAuthenticationToken( dados.login(), dados.senha() );
+
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+}
+
+
---
+++ src/main/java/br/edu/ifsp/prw3/api_2024_2/usuario/dadosAutenticacao.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.prw3.api_2024_2.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record dadosAutenticacao(
+
+        @NotBlank
+        String login,
+
+        @NotBlank
+        String senha) {
+
+}
+
+
